d6806fe42dc97c0f00ed6500308a9bca0b1f980d
Han Wang <hanwang.0501@gmail.com>
2018-04-17 16:53:35 +0800

    first commit

---
+++ config_training.py
@@ -0,0 +1,4 @@
+config = {'preprocess_result_path':'/root/workspace/preprocessing_4_9/', 
+          'bbox_path':'../detector/results/res18/bbox/',
+          'preprocessing_backend':'python'
+    }
